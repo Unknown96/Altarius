7184118ab20f0b5401cf18767ee72edd2970aa9b
Unknown96 <roobin.k65@gmail.com>
2017-09-21 19:24:15 +0200

    Beta 0.2

--- js/main.js
+++ js/main.js
@@ -32,7 +32,37 @@ $(document).ready(function() {
 
 	$('.slider').hover(function() {
 		$('.slider').find('h1').slideToggle(400);
+
 	})
+
+	$('.author').click(function() {
+		$('.blur p').html('Author: Robin Kastorp<br>Contact: Altarius@gmail.com')
+		$('.blur').fadeToggle(400);
+
+	});
+
+	$('.blur span').click(function() {
+	var notify = $('.blur')
+		notify.fadeToggle(400);
+		notify.find('button').hide()
+		notify.find('input').hide()
+	});
+
+	$('.admin').click(function() {
+		var notify = $('.blur')
+		notify.find('p').html('Login using your access key.')
+		notify.find('input').css('display', 'block')
+		notify.find('button').show()
+		$('.blur').fadeToggle(400);
+	});
+
+	$('.blur .cancel').click(function() {
+	var notify = $('.blur')
+		notify.fadeToggle(400);
+		notify.find('button').hide()
+		notify.find('input').hide()
+	});
+
 });
 
 
